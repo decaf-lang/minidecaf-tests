b6a1cd2286de0be76b05122ab1683121688e3b66
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2020-08-11 22:12:42 +0800

    some step4 failed tests could be ignored

--- test_examples.sh
+++ test_examples.sh
@@ -1,11 +1,12 @@
 for i in examples/*/*.c
 do
-    gcc -w -m32 $i             #compile with gcc
-    ./a.out                 	#run it
+    riscv64-unknown-elf-gcc $i             #compile with gcc
+    qemu-riscv64 ./a.out                 	#run it
     expected=$?             	#get exit code
-    gcc -w -m32 -o out $i      #compile with YOUR COMPILER or some shell script with YOUR COMPILER
+    /media/chyyuu/ca8c7ba6-51b7-41fc-8430-e29e31e5328f/thecode/rust/compilers/minidecaf/target/debug/minidecaf $i  >> asm.S 
+    riscv64-unknown-elf-gcc asm.S -o out   #compile with YOUR COMPILER or some shell script with YOUR COMPILER
     base="${i%.*}"
-    ./out                      #run the thing we assembled
+    qemu-riscv64 ./out                      #run the thing we assembled
     actual=$?                  #get exit code
     echo -n "$i:    "
     if [ "$expected" -ne "$actual" ]
@@ -14,7 +15,7 @@ do
     else
         echo "OK"
     fi
-    rm out a.out
+    rm -f out a.out asm.S 2>/dev/null
 done
 
 #tested in ubutnu 20.04 x86-64
