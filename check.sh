622243f420977b7e91217289eb6ededf8f802098
Hoblovski <daizy19@mails.tsinghua.edu.cn>
2020-08-20 16:50:40 +0800

    fix tests

--- check.sh
+++ check.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-export CC=riscv64-unknown-elf-gcc
+export CC="riscv64-unknown-elf-gcc -Dint=long"
 export QEMU=qemu-riscv64
-JOBS=(examples/*/*.c)
+JOBS=(examples/step{1,2,3,4,5,6,7,8,9,10,11,12}/*.c)
 
 
 gen_asm() {
     cfile=$1
     asmfile=$2
-    export PYTHONPATH=/home/hob/Programs/ta-courses/cp2020/src/minidecaf
-    python -m minidecaf $cfile $asmfile
+
+    # 如果是 python 参考代码，下面反注释
+    # PYTHONPATH=../minidecaf python -m minidecaf $cfile $asmfile
+
+    # 如果是 rust 参考代码，下面反注释
+    # ../minidecaf/target/debug/minidecaf $cfile > $asmfile
+
+    # 你自己写的编译器，仿照上面自行添加命令
 }
 export -f gen_asm
 
@@ -16,6 +22,8 @@ export -f gen_asm
 run_job() {
     infile=$1
     outbase=${infile%.c}
+
+    rm $outbase.{gcc,expected,err,my,actual,s} 1>/dev/null 2>&1
 
     $CC $infile -o $outbase.gcc
     $QEMU $outbase.gcc
@@ -27,7 +35,11 @@ run_job() {
     ) >$outbase.err 2>&1
     if [[ $? != 0 ]]; then
         echo "ERR ${infile}"
-        return 1
+        if [[ `cat $outbase.err` = "error: gen_asm undefined" ]]; then
+            return 3
+        else
+            return 2
+        fi
     fi
     $QEMU $outbase.my
     echo $? > $outbase.actual
@@ -37,9 +49,6 @@ run_job() {
         return 1
     else
         echo "OK ${infile}"
-        if [[ $((RANDOM % 10)) = 10 ]]; then
-            return 1;
-        fi
         return 0
     fi
 }
@@ -70,6 +79,11 @@ check_env_and_parallel() {
     fi
 }
 
+
+if ! [[ -d ../minidecaf ]]; then
+    echo "Put your code in ../minidecaf"
+    exit 1
+fi
 
 if check_env_and_parallel; then
     parallel run_job ::: ${JOBS[@]}
