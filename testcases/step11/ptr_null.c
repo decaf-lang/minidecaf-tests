794fa8ffc8440bf53c9f7d3334a222dc50b55dc3
equation314 <equation618@gmail.com>
2020-08-27 17:26:01 +0800

    update step11/ptr_null.c, move step12/cast.c to step11

--- testcases/step11/ptr_null.c
+++ testcases/step11/ptr_null.c
@@ -3,15 +3,16 @@ int *gp;
 int main() {
     int a=456;
     int b=789;
-    int *np=0;
-    np=0;
+    int *np=(int *)0;
+    np=( int  * )(int * *) - (int) 0;
+    if (np != (int*)0) return 244;
     if (np != gp) return 244;
     if (np == &a) return 244;
     if (np == &g) return 244;
     if (&a == &g) return 244;
     if (&a == &b) return 244;
     if (&b == &g) return 244;
-    a=23;
+    if (np == (int*)0) a=23;
     np=&a;
     return*np;
 }
