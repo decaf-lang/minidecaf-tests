e9f67b4372c1e98c6c10db7b9e39708f251a4314
namasikanam <namasikanam@gmail.com>
2021-08-16 20:53:22 +0800

    add std=c17 to gcc option

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CC="riscv64-unknown-elf-gcc  -march=rv32im -mabi=ilp32"
+export CC="riscv64-unknown-elf-gcc -std=c17 -march=rv32im -mabi=ilp32"
 export QEMU=qemu-riscv32
 export SPIKE="spike --isa=RV32G /usr/local/bin/pk"
 if [ $(uname) = "Linux" ]; then
