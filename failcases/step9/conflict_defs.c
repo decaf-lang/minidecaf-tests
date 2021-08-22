d5e33fbe8df9d9c148122a9f64032949a44ae43a
namasikanam <namasikanam@gmail.com>
2021-08-23 06:10:15 +0800

    failcases for step9

---
+++ failcases/step9/conflict_defs.c
@@ -0,0 +1,12 @@
+int f(int x) {
+    return x + 1;
+}
+
+int f(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    return 0;
+}
+
---
+++ failcases/step9/same_param_in_func_def.c
@@ -0,0 +1,2 @@
+int f(int x, int x) { return x + x; }
+int main() { return f(1, 2); }
